c522c0c8acc233721de700961ec7b4e3891e152d
Tobiasz <tobi13072@gmail.com>
2024-02-13 18:10:43 +0100

    init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {HeartComponent} from "./components/heart/heart.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'heart', component: HeartComponent}
+];
